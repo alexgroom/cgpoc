a36a8792209421a90674c10ad84a07fa026b193f
Alex Groom <agroom@redhat.com>
2020-07-23 18:15:49 +0100

    add pipeline resources

--- agcoolstorepipeline/setup.sh
+++ agcoolstorepipeline/setup.sh
@@ -1,10 +1,10 @@
 #
 # Simple tekton pipeline to build all components
 #
-#oc new-project agcoolstorepipeline 
+oc new-project agcoolstorepipeline 
 oc apply -f deployments/catalog.yaml
 oc apply -f deployments/catalog.yaml
 oc apply -f deployments/catalog.yaml
 oc apply -f deployments/catalog.yaml
 oc apply -f simple_pipeline.yaml
-
+oc apply -f pipelineresources.yaml
