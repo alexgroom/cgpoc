dedec98155b9e2b0d5313e453e89c8a9f9f360a6
Alex Groom <agroom@redhat.com>
2020-07-24 12:33:54 +0100

    Fix duplicate catalogs

--- agcoolstorepipeline/setup.sh
+++ agcoolstorepipeline/setup.sh
@@ -3,8 +3,8 @@
 #
 oc new-project agcoolstorepipeline 
 oc apply -f deployments/catalog.yaml
-oc apply -f deployments/catalog.yaml
-oc apply -f deployments/catalog.yaml
-oc apply -f deployments/catalog.yaml
+oc apply -f deployments/inventory.yaml
+oc apply -f deployments/gateway.yaml
+oc apply -f deployments/web.yaml
 oc apply -f simple_pipeline.yaml
 oc apply -f pipelineresources.yaml
