6ca6f314f857e216d8db1d43c4acf2cec025c887
Alex Groom <agroom@redhat.com>
2020-07-24 16:13:44 +0100

    Add grafana and fix virtual service

--- agcoolstoresm/monitoring.sh
+++ agcoolstoresm/monitoring.sh
@@ -1,5 +1,6 @@
 # monitoring configuration
 oc project agcoolstoresm
+# Add Promethues to the project
 # Create a config map
 oc create configmap prom --from-file=prometheus.yml
 #
@@ -10,3 +11,9 @@ oc expose svc/prometheus
 # Mount the map into the container 
 oc set volume dc/prometheus --add -t configmap --configmap-name=prom -m /etc/prometheus/prometheus.yml --sub-path=prometheus.yml
 oc rollout status -w dc/prometheus
+#
+# Add Grafana (credentials: admin/admin)
+#
+oc new-app grafana/grafana --name grafana
+oc expose svc/grafana
+
