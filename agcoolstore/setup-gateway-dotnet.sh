ebe09cab9f741bdec541dfd67ee458cf0f7a7cea
Alex Groom <agroom@redhat.com>
2021-01-04 12:25:54 +0000

    Add script for gateway-dotnet

---
+++ agcoolstore/setup-gateway-dotnet.sh
@@ -0,0 +1,11 @@
+# script to install agcoolstore components
+oc project agcoolstore
+oc delete all --selector app=gateway
+# create gateway and apply environment variables
+oc new-app dotnet:3.1~https://github.com/alexgroom/cnw3.git --context-dir=gateway-dotnet --name=gateway --as-deployment-config\
+   -l app.openshift.io/runtime=dotnet \
+  -e COMPONENT_CATALOG_HOST=catalog -e COMPONENT_INVENTORY_HOST=inventory -e COMPONENT_CATALOG_PORT=8080 -e COMPONENT_INVENTORY_PORT=8080
+oc expose svc gateway
+# Add component labels to group services
+oc label dc gateway app.kubernetes.io/part-of=coolstore
+
