d5a086176c9be97de5917dc1ee926f038c43513b
Alex Groom <agroom@redhat.com>
2021-06-24 15:54:40 +0100

    Create pvs-sno.sh

---
+++ pvs-sno.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+mkdir -p /var/pocfs/user-vols/pv{1..20}
+echo "Creating PV for users.."
+for pvnum in {1..20} ; do   echo "/var/pocfs/user-vols/pv${pvnum} *(rw,root_squash)" >> /etc/exports.d/openshift-uservols.exports;   chown -R nfsnobody.nfsnobody /var/pocfs;   chmod -R 777 /var/pocfs; done
+systemctl restart nfs-server
+#
+export nfshost=192.168.0.201
+mkdir -p pvs
+export volsize="10Gi"
+
+for volume in pv{11..20} ; do
+cat << EOF > pvs/${volume}
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "${volume}"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "${volsize}"
+    },
+    "accessModes": [ "ReadWriteMany" ],
+    "nfs": {
+        "path": "/var/pocfs/user-vols/${volume}",
+        "server": "${nfshost}"
+    },
+    "persistentVolumeReclaimPolicy": "Retain"
+  }
+}
+EOF
+echo "Created def file for ${volume}";
+done;
+#
+export volsize="5Gi"
+for volume in pv{1..10} ; do
+cat << EOF > pvs/${volume}
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "${volume}"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "${volsize}"
+    },
+    "accessModes": [ "ReadWriteOnce" ],
+    "nfs": {
+        "path": "/var/pocfs/user-vols/${volume}",
+        "server": "${nfshost}"
+    },
+    "persistentVolumeReclaimPolicy": "Recycle"
+  }
+}
+EOF
+echo "Created def file for ${volume}";
+done;
+systemctl restart nfs-server
+cat pvs/* | oc create -f -
