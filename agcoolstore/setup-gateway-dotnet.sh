287774e1d7aeff81ae5f49f52f66e06136ae901a
Alex Groom <alex.groom@gmail.com>
2024-02-21 16:51:18 +0000

    remove dc

--- agcoolstore/setup-gateway-dotnet.sh
+++ agcoolstore/setup-gateway-dotnet.sh
@@ -1,10 +1,10 @@
 # script to install agcoolstore components
 oc delete all --selector app=gateway
 # create gateway and apply environment variables
-oc new-app dotnet:6.0~https://github.com/alexgroom/cnw3.git --context-dir=gateway-dotnet --name=gateway --as-deployment-config\
+oc new-app dotnet:6.0~https://github.com/alexgroom/cnw3.git --context-dir=gateway-dotnet --name=gateway \
    -l app.openshift.io/runtime=dotnet \
   -e COMPONENT_CATALOG_HOST=catalog -e COMPONENT_INVENTORY_HOST=inventory -e COMPONENT_CATALOG_PORT=8080 -e COMPONENT_INVENTORY_PORT=8080
 oc expose svc gateway
 # Add component labels to group services
-oc label dc gateway app.kubernetes.io/part-of=coolstore
+oc label deployment gateway app.kubernetes.io/part-of=coolstore
 
--- agcoolstore/setup-noproject.sh
+++ agcoolstore/setup-noproject.sh
@@ -1,39 +1,42 @@
 # script to install agcoolstore components
-oc new-app java:11~https://github.com/alexgroom/cnw3.git --context-dir=catalog-spring-boot --name=catalog  -l app.openshift.io/runtime=spring --as-deployment-config
+oc new-app java:11~https://github.com/alexgroom/cnw3.git --context-dir=catalog-spring-boot --name=catalog  -l app.openshift.io/runtime=spring 
 oc expose svc catalog
 #
 # Build the inventory variant usig the external mariadb
-oc new-app java:11~https://github.com/alexgroom/cnw3.git --context-dir=inventory-quarkus --name=inventory  -l app.openshift.io/runtime=quarkus --as-deployment-config \
+oc new-app java:11~https://github.com/alexgroom/cnw3.git --context-dir=inventory-quarkus --name=inventory  -l app.openshift.io/runtime=quarkus  \
   --build-env=QUARKUS_DATASOURCE_DB_KIND=mariadb --build-env=QUARKUS_DATASOURCE_JDBC_URL=jdbc:mariadb://inventory-mariadb:3306/inventorydb \
   --build-env=QUARKUS_DATASOURCE_USERNAME=inventory --build-env=QUARKUS_DATASOURCE_PASSWORD=inventory
 
 oc expose svc inventory
 # create gateway and apply environment variables
-oc new-app java:11~https://github.com/alexgroom/cnw3.git --context-dir=gateway-vertx --name=gateway --as-deployment-config\
+oc new-app java:11~https://github.com/alexgroom/cnw3.git --context-dir=gateway-vertx --name=gateway \
    -l app.openshift.io/runtime=vertx \
   -e COMPONENT_CATALOG_HOST=catalog -e COMPONENT_INVENTORY_HOST=inventory -e COMPONENT_CATALOG_PORT=8080 -e COMPONENT_INVENTORY_PORT=8080
 oc expose svc gateway
-oc new-app https://github.com/alexgroom/cnw3.git --context-dir=web-nodejs --name=web -l app.openshift.io/runtime=nodejs --as-deployment-config
+oc new-app https://github.com/alexgroom/cnw3.git --context-dir=web-nodejs --name=web -l app.openshift.io/runtime=nodejs 
 oc expose svc web
 oc new-app dotnet:6.0~https://github.com/alexgroom/inventory-api-1st-dotnet.git --context-dir=src/Coolstore.Inventory --name=inventory-dotnet \
-  -l app.openshift.io/runtime=dotnet --as-deployment-config
+  -l app.openshift.io/runtime=dotnet 
 oc expose svc inventory-dotnet
 # Add component labels to group services
-oc label dc gateway app.kubernetes.io/part-of=coolstore
-oc label dc catalog app.kubernetes.io/part-of=coolstore
-oc label dc inventory app.kubernetes.io/part-of=coolstore
-oc label dc web app.kubernetes.io/part-of=coolstore
-oc label dc inventory-dotnet app.kubernetes.io/part-of=coolstore
+oc label deployment gateway app.kubernetes.io/part-of=coolstore
+oc label deployment catalog app.kubernetes.io/part-of=coolstore
+oc label deployment inventory app.kubernetes.io/part-of=coolstore
+oc label deployment web app.kubernetes.io/part-of=coolstore
+oc label deployment inventory-dotnet app.kubernetes.io/part-of=coolstore
 # add databases components for inventory and catalog
 #
-oc process -n openshift postgresql-ephemeral --param=DATABASE_SERVICE_NAME=catalog-postgresql \
+PROJECT_NAME=$(oc project -q)
+oc get template -n openshift postgresql-ephemeral -o yaml | sed "s/namespace: openshift/namespace: ${PROJECT_NAME}/" | \
+oc process  --param=DATABASE_SERVICE_NAME=catalog-postgresql \
         --param=POSTGRESQL_DATABASE=catalogdb --param=POSTGRESQL_USER=catalog \
         --param=POSTGRESQL_PASSWORD=catalog \
         --labels=app=catalog \
         --labels=app.openshift.io/runtime=postgresql \
           | oc create -f -
 
-oc process -n openshift mariadb-ephemeral --param=DATABASE_SERVICE_NAME=inventory-mariadb \
+oc get template -n openshift mariadb-ephemeral  -o yaml | sed "s/namespace: openshift/namespace: ${PROJECT_NAME}/" | \
+oc process  --param=DATABASE_SERVICE_NAME=inventory-mariadb \
         --param=MYSQL_DATABASE=inventorydb --param=MYSQL_USER=inventory \
         --param=MYSQL_PASSWORD=inventory --param=MYSQL_ROOT_PASSWORD=inventoryadmin \
         --labels=app=inventory \
